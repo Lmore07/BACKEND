af978c0296a174435aed3a2e61fc1c2a6f61b44b
Lmore07 <lmoreirat@uteq.edu.ec>
2022-10-13 17:06:12 -0500

    perfiles-usuarios

--- src/modules/routes.ts
+++ src/modules/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import rutaBD from './base-datos/routers'
+import rutaPerfilesUsuarios from './perfiles-usuarios/routers'
 
 const applyRoutes=(app:Application) =>{
-    app.use("/api/base-datos",rutaBD)
+    app.use("/api/base-datos",rutaBD),
+    app.use("/api/perfiles-usuarios",rutaPerfilesUsuarios)
 };
 
 export default applyRoutes;
